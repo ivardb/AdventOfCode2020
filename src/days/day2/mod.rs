b10e1f4e9b2ec48079b434de69b4060843640a70
icdebruin <I.C.debruin@student.tudelft.nl>
2020-12-02 08:28:34 +0100

    Day 2 v1

--- src/days/day2/mod.rs
+++ src/days/day2/mod.rs
@@ -1,6 +1,48 @@
+use std::str::FromStr;
+use std::num::ParseIntError;
+
 pub mod part1;
 pub mod part2;
 
 pub fn default_input() -> String {
     String::from(include_str!("input"))
+}
+
+struct Row {
+    min: i32,
+    max: i32,
+    letter: char,
+    password: String,
+}
+
+impl FromStr for Row {
+    type Err = ParseIntError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let split : Vec<&str> = s.split_ascii_whitespace().collect();
+        let numsplit : Vec<&str> = split[0].split("-").collect();
+        let min : i32 = numsplit[0].trim().parse()?;
+        let max : i32 = numsplit[1].trim().parse()?;
+        let chars : Vec<char> = split[1].trim().chars().collect();
+        let letter = chars[0];
+        let password = String::from(split[2].trim());
+        Ok(Row {min, max, letter, password})
+    }
+}
+
+impl Row {
+    fn is_valid(&self) -> bool {
+        let mut letter_count = 0;
+        for c in self.password.chars() {
+            if c == self.letter {
+                letter_count += 1;
+            }
+        }
+        letter_count <= self.max && letter_count >= self.min
+    }
+
+    fn is_valid2(&self) -> bool {
+        let chars : Vec<char> = self.password.chars().collect();
+        (chars[(self.min-1) as usize] == self.letter) ^ (chars[(self.max-1) as usize] == self.letter)
+    }
 }
--- src/days/day2/part2.rs
+++ src/days/day2/part2.rs
@@ -1,3 +1,11 @@
-pub fn run() {
+use crate::days::day2::Row;
+use std::str::FromStr;
 
+pub fn run() {
+    let input = include_str!("input");
+    println!("{}", password_validator_str(input).unwrap());
+}
+
+pub fn password_validator_str(input : &str) -> Result<usize, ()> {
+    Ok(input.lines().map(|l| {Row::from_str(l).unwrap()}).filter(Row::is_valid2).count())
 }
