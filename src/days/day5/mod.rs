39726bbc85aced1d1f5e5c290af26ee357a93f73
icdebruin <I.C.debruin@student.tudelft.nl>
2020-12-04 16:55:02 +0100

    Setup day 5

---
+++ src/days/day5/mod.rs
@@ -0,0 +1,11 @@
+pub mod part1;
+pub mod part2;
+
+pub fn default_input()  -> &'static str {
+    include_str!("input")
+}
+
+pub fn run() {
+    part1::run();
+    part2::run();
+}
--- src/lib.rs
+++ src/lib.rs
@@ -9,6 +9,7 @@ pub fn run(day: usize) {
         2 => day2::run(),
         3 => day3::run(),
         4 => day4::run(),
+        5 => day5::run(),
         _ => {
             println!("Day not valid");
             return
