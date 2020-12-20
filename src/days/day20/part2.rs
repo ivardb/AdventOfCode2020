fef6b984e9c602e424c1dbf88dc77a41d98a4a89
icdebruin <I.C.debruin@student.tudelft.nl>
2020-12-20 20:14:52 +0100

    Finish cleanup

--- src/days/day20/part2.rs
+++ src/days/day20/part2.rs
@@ -17,14 +17,17 @@ pub fn picture(pictures : HashMap<i64, Picture>) -> Result<usize, ()> {
 
     let border_map = compute_borders(&pictures, picture_size);
 
-    let mut matches : HashMap<BorderId, Vec<Match>> = HashMap::new();
-    let mut reversed : HashMap<BorderId, Vec<char>> = HashMap::new();
+    //Compute all matches between borders
+    let mut matches : HashMap<BorderId, Vec<BorderId>> = HashMap::new();
     for p1 in pictures.values() {
         for (i, b1) in border_map.get(&p1.id).unwrap().iter().enumerate() {
             let border_id = BorderId::from_ids(p1.id, i);
+
+            //Create reverse
             let mut reverse = b1.clone();
             reverse.reverse();
-            reversed.insert(border_id, reverse);
+
+            //Ensure empty vectors to filter out edges later
             matches.insert(border_id, Vec::new());
 
             for p2 in pictures.values() {
@@ -33,67 +36,73 @@ pub fn picture(pictures : HashMap<i64, Picture>) -> Result<usize, ()> {
                 }
                 for (j, b2) in border_map.get(&p2.id).unwrap().iter().enumerate() {
                     let border_id2 = BorderId::from_ids(p2.id, j);
-                    if b2 == b1 {
-                        let matched = Match {
-                            matched: border_id2,
-                            reversed: false
-                        };
+                    if (b2 == b1) | (b2 == &reverse) {
                         matches.entry(border_id)
-                            .and_modify(|v| v.push(matched));
-                    }
-                    if b2 == reversed.get(&border_id).unwrap() {
-                        let matched = Match {
-                            matched: border_id2,
-                            reversed: true
-                        };
-                        matches.entry(border_id)
-                            .and_modify(|v| v.push(matched));
+                            .and_modify(|v| v.push(border_id2));
                     }
                 }
             }
         }
     }
+    //Filter out edges
     let edges : Vec<_> = matches.iter().filter(|(_id, matches)| matches.len() == 0).map(|(id, _matches)| *id).collect();
+
+    //Get count per picture id to get corners
     let mut count = HashMap::new();
     for id in &edges {
         *count.entry(id.id).or_insert(0) += 1;
     }
-    let corners : Vec<_> = count.iter().filter(|(_k,v)| **v == 2).map(|(k, _v)| *k).sorted().collect();
+    let corners : Vec<_> = count.iter().filter(|(_k,v)| **v == 2).map(|(k, _v)| *k).collect();
 
-
+    //Get starting corner
     let start_corner = corners[0];
 
-
+    //Sort by key so top edge becomes easy to get to the top.
     let corner_edges = edges.iter().filter(|b| b.id == start_corner).sorted_by_key(|e| e.side.to_index()).collect::<Vec<&BorderId>>();
-    let start_edge = corner_edges[1];
     let top_edge = corner_edges[0];
+    let start_edge = corner_edges[1];
+
+    //Rotate picture according to orientations computed from the location of the edges
     let orientation = get_orientation(top_edge.side, start_edge.side);
-    let rotated = orientation.iter().fold(pictures.get(&start_corner).unwrap().pixels.clone(), |p, o| orientate(p, *o));
+    let rotated = orientation.iter()
+        .fold(pictures.get(&start_corner).unwrap().pixels.clone(), |p, o| orientate(p, *o));
+
+    //Aggregation for pictures split by column. Easiest for computation.
     let mut picture_cols: Vec<Vec<Grid<char>>> = Vec::new();
     for _i in 0..dimension {
         picture_cols.push(Vec::new());
     }
     picture_cols[0].push(rotated.clone());
+
+    //The id of the last column of the previous picture
     let mut last_col_id = start_edge.opposite();
+    //The actual col
     let mut last_col = rotated.iter_col(rotated.cols() - 1).map(|c| *c).collect_vec();
+    //Ids of the top of each column, required for later
     let mut ids = Vec::new();
     ids.push(start_corner);
+
+    //Setup initial row
     for i in 1..dimension {
+        //Get the matching picture and try all rotations until it matches
         let matched = matches.get(&last_col_id).unwrap()[0];
-        let matched_grid = pictures.get(&matched.matched.id).unwrap().pixels.clone();
+        let matched_grid = pictures.get(&matched.id).unwrap().pixels.clone();
         for orientation in possible_orientations() {
             let rotate = orientation.iter().fold(matched_grid.clone(), |p, o| orientate(p, *o));
             let first_col = rotate.iter_col(0).map(|c| *c).collect_vec();
+
+            //Once it matches update all variables for the next round
             if first_col == last_col {
                 last_col = rotate.iter_col(rotate.cols() -1).map(|c| *c).collect_vec();
-                last_col_id = matched.matched.opposite();
+                last_col_id = matched.opposite();
                 picture_cols[i].push(rotate);
-                ids.push(matched.matched.id);
+                ids.push(matched.id);
                 break;
             }
         }
     }
 
+    //For each column we will get the information from the top row and then expand the column downwards
     for i in 0..dimension {
         let old_pic = &picture_cols[i][0];
         let mut last_row = old_pic.iter_row(old_pic.rows() - 1).map(|c| *c).collect_vec();
@@ -102,19 +111,21 @@ pub fn picture(pictures : HashMap<i64, Picture>) -> Result<usize, ()> {
         let mut last_row_id = BorderId {id, side};
         for _j in 1..dimension {
             let matched = matches.get(&last_row_id).unwrap()[0];
-            let matched_grid = pictures.get(&matched.matched.id).unwrap().pixels.clone();
+            let matched_grid = pictures.get(&matched.id).unwrap().pixels.clone();
             for orientation in possible_orientations() {
                 let rotate = orientation.iter().fold(matched_grid.clone(), |p, o| orientate(p, *o));
                 let first_row = rotate.iter_row(0).map(|c| *c).collect_vec();
                 if first_row == last_row {
                     last_row = rotate.iter_row(rotate.rows() - 1).map(|c| *c).collect_vec();
-                    last_row_id = matched.matched.opposite();
+                    last_row_id = matched.opposite();
                     picture_cols[i].push(rotate);
                     break;
                 }
             }
         }
     }
+
+    //Construct the final picture without borders from the correctly orientated pictures.
     let mut picture = Vec::new();
     for ygrid in 0..dimension {
         for j in 1..picture_size -1 {
@@ -264,12 +275,6 @@ enum Orientation {
     FlipHorizontal,
     RotateLeft,
     RotateRight
-}
-
-#[derive(PartialEq, Eq, Hash, Copy, Clone, Debug)]
-struct Match {
-    matched : BorderId,
-    reversed: bool
 }
 
 #[derive(PartialEq, Eq, Hash, Copy, Clone, Debug)]
